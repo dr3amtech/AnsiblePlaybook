d758688f14beaecf71c5eca147891e3eda271235
joshua joseph <joshuajoseph7@yahoo.com>
2018-10-31 00:52:01 +0100

    MOD

--- AnsiblePlaybook/HttpRequestHook.py
+++ AnsiblePlaybook/HttpRequestHook.py
@@ -2,7 +2,7 @@ import requests
 
 
 try:
-	url = "http://joshuajoseph7:113f2657f84fc5ca88f7004e9cd155766f@localhost:8080/job/Humanoid_Playground/build?token=JENKINSBUILDFREE"
+	url = "http://joshuajoseph7:113f2657f84fc5ca88f7004e9cd155766f@localhost:8080/job/FirstPipline/build?token=JENKINSBUILDFREE"
 	req = requests.get(url)
 except requests.exceptions.RequestException as e:  # This is the correct syntax
     print e
